7ff7588267f8f3e4b6616ac425a32b1d3857ed8e
alucard93 <m.vinicius93.brasilmortal@gmail.com>
2022-11-26 16:07:48 -0300

    Update estudo.wb.py

--- estudo.wb.py
+++ estudo.wb.py
@@ -54,7 +54,7 @@ def remove_more_than_two_repetitions(text: str):
 
 result = remove_more_than_two_repetitions('Ollloco meuuuu, taaa peegaando fogoo biiiiichooo')
 
-print(result)
+# print(result)
 
 x = 'Arroz com feijão e batata frita porra'
 
@@ -64,6 +64,25 @@ x = 'Arroz com feijão e batata frita porra'
 #     print('nã está porra')
 
 my_string = 'ola m5'
-for char in my_string:
+# for char in my_string:
+#     if char != " ":
+#         print(char)
+
+# for i, char in enumerate(my_string):
+#     print(char, i)
+
+my_string1 = 'ola m5'
+arr = []
+for char in my_string1:
     if char != " ":
-        print(char)
+        arr.append(char)
+# print("".join(arr))
+
+my_string2 = 'olam5'
+fat1 = my_string2[0:3]
+fat2 = my_string2[3:]
+
+jun = fat1 + ' ' + fat2
+
+print(jun)
+print(jun.split())
