463dc2021fca05324760a4019956ba451e187473
alucard93 <m.vinicius93.brasilmortal@gmail.com>
2022-11-28 19:06:46 -0300

    Update erro.py

--- erro.py
+++ erro.py
@@ -1,6 +1,17 @@
 import os 
 os.system('cls')
 
+def div_by_zero():
+    try:
+        a = 1
+        b = 0
+        print(a / b)
+
+    except ZeroDivisionError:
+        print('não é possivel dividir por zero')
+
+div_by_zero()
+
 def unexisting_key():
     try:
         my_dict = {"name": "Alex", "module": "M5"}
@@ -9,3 +20,23 @@ def unexisting_key():
     
     except KeyError:
         print('chave não encontrada')
+
+unexisting_key()
+
+def unexisting_index():
+    try:
+        my_list = [0, 1]
+        print(my_list[5])
+
+    except IndexError:
+        print('indice não encontrado')
+
+unexisting_index()
+
+
+def misterious_error():
+    try:
+        a = 5
+        print(a.capitalize())
+    except AttributeError:
+        print('AttributeError tratado')
